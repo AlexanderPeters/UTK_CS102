429c33f4134ef472573af7dbc787edb09de6797a
Alexander Peters <businessonly1011@gmail.com>
2019-01-15 10:23:17 -0500

    First commit for this repo

---
+++ cs102/lab1/lab1.cpp
@@ -0,0 +1,21 @@
+// Alexander Peters
+// Lab 1
+// Due 1.9.19
+// Description:
+// The first cs102 lab.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	string name;
+
+	cout << "What is your name? ";
+
+	cin >> name;
+
+	cout << "Hello, " << name << "\n";
+	
+	return 1;
+}
+
---
+++ cs102/lecture/LectureNotes1.cpp
@@ -0,0 +1,21 @@
+// Alexander Peters
+// Lecture Notes 1
+// 01/15/19
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	string first_name;
+	string last_name;
+
+	cout << "Enter you first and last name: ";
+	cin >> first_name;
+	cin >> last_name;
+
+	cout << "Your name is " << first_name
+		 << " " << last_name << '\n';
+
+	return 0;
+}	
+
