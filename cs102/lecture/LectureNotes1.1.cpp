c8d3e5a28a919fff2e4f064e9080fa5bd1633c27
Alexander Peters <businessonly1011@gmail.com>
2019-01-15 10:52:37 -0500

    End of Lecture Commit 01/15/19

---
+++ cs102/lecture/LectureNotes1.1.cpp
@@ -0,0 +1,20 @@
+// Alexander Peters
+// Lecture 1.1
+// 01/15/19
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	char a;
+	int b;
+	float c;
+
+	cin >> a >> b >> c;
+
+	cout << a << '\n'
+		 << b << '\n'
+		 << c << '\n';
+
+	return 0;
+}
---
+++ cs102/lecture/LectureNotes1.2.cpp
@@ -0,0 +1,15 @@
+// Alexander Peters
+// Lecture 1.2
+// 01/15/19
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Size of int: " << sizeof(int) << ".\n";
+	cout << "Size of long: " << sizeof(long) << ".\n";
+	cout << "Size of long_long: " << sizeof(long long) << ".\n";
+
+	return 0;
+}
+
